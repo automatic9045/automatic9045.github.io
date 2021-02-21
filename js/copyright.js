da7af0a8f6dfe474d14d878eb3a8d16f88dfe1c7
Automatic9045 <ayax.tk@gmail.com>
2021-02-21 22:51:38 +0900

    initial commit

---
+++ js/copyright.js
@@ -0,0 +1,2 @@
+﻿const year = new Date().getFullYear();
+document.writeln("&copy; " + (year == 2021 ? "2021" : "2021 - " + year) + " Automatic9045");
---
+++ js/wait.js
@@ -0,0 +1,3 @@
+﻿function wait(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
