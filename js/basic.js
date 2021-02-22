433487fe7b5e5af9e050401bdc2563a36de4c89e
Automatic9045 <ayax.tk@gmail.com>
2021-02-23 00:58:35 +0900

    jsを修正

--- js/basic.js
+++ js/basic.js
@@ -2,14 +2,14 @@
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function include(path) {
+function include(path, elementId) {
 	const xhr = new XMLHttpRequest();
 
 	xhr.open("GET", path, true);
 	xhr.onreadystatechange = function () {
 		if (xhr.readyState === 4 && xhr.status === 200) {
 			var responseText = xhr.responseText;
-			document.write(responseText);
+			document.getElementById(elementId).innerHTML = responseText;
 		}
 	};
 	xhr.send();
