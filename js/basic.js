89148f0681482affe32d69ef220743823afb8abc
Automatic9045 <ayax.tk@gmail.com>
2021-02-23 00:42:08 +0900

    jsを修正

---
+++ js/basic.js
@@ -0,0 +1,16 @@
+﻿function wait(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function include(path) {
+	const xhr = new XMLHttpRequest();
+
+	xhr.open("GET", path, true);
+	xhr.onreadystatechange = function () {
+		if (xhr.readyState === 4 && xhr.status === 200) {
+			var responseText = xhr.responseText;//String型で取得
+			document.writeIn(responseText);//完了
+		}
+	};
+	xhr.send();
+}
--- js/wait.js
+++
@@ -1,3 +0,0 @@
-﻿function wait(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
