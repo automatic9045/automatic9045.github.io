da04ccb0e84a15c8b188001f9bd086b9825950f4
Automatic9045 <ayax.tk@gmail.com>
2021-02-23 01:17:24 +0900

    index.htmlのヘッダーインクルードをjsに完全移行

--- js/basic.js
+++ js/basic.js
@@ -2,15 +2,29 @@
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function include(path, elementId) {
+function include(path, element) {
 	const xhr = new XMLHttpRequest();
 
 	xhr.open("GET", path, true);
 	xhr.onreadystatechange = function () {
 		if (xhr.readyState === 4 && xhr.status === 200) {
 			var responseText = xhr.responseText;
-			document.getElementById(elementId).innerHTML = responseText;
+			element.innerHTML = responseText;
 		}
 	};
 	xhr.send();
+}
+
+function includeById(path, elementId) {
+	const element = document.getElementById(elementId);
+	include(path, element);
+}
+
+function includeByClasses(path, elementClasses, index) {
+	const element = document.getElementByClassName(elementClasses)[index];
+	include(path, element);
+}
+
+function includeByClasses(path, elementClasses) {
+	includeByClasses(path, elementClasses, 0);
 }
