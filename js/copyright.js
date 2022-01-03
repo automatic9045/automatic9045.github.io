e874f3540ab71735199557b186a5be9a65b028ce
Automatic9045 <67314487+Automatic9045@users.noreply.github.com>
2022-01-03 16:58:52 +0900

    change: 著作権表示を微調整

--- js/copyright.js
+++ js/copyright.js
@@ -1,6 +1,6 @@
 ﻿const year = new Date().getFullYear();
 const element = document.createElement("span");
-const content = "&copy; " + (year == 2021 ? "2021" : "2021 - " + year) + " Automatic9045";
+const content = "&copy; " + (year == 2021 ? "2021" : "2021-" + year) + " Automatic9045";
 element.innerHTML = content;
 const scripts = document.getElementsByTagName('script');
 scripts[scripts.length - 1].parentNode.appendChild(element);
