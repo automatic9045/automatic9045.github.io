f92a5ea9f1cfa9f23f4fd506f4327ee6ac2f156d
Automatic9045 <67314487+Automatic9045@users.noreply.github.com>
2022-01-12 15:40:14 +0900

    update: 直近にアクセスしている場合、スプラッシュを表示しない仕様に変更

---
+++ js/index.js
@@ -0,0 +1,25 @@
+﻿function prepareToFade() {
+    if (Cookies.get("do-fade") != "1") {
+        const loadingDiv = document.getElementById("loading");
+        const bodyDiv = document.getElementById("body");
+
+        loadingDiv.classList.add("will-fade");
+        bodyDiv.classList.add("will-fade");
+    }
+}
+
+async function fade() {
+    const bodyDiv = document.getElementById("body");
+
+    if (bodyDiv.classList.contains("will-fade")) {
+        await wait(200);
+        document.getElementById("loading-mes-loading").classList.add("fade");
+        document.getElementById("loading-mes-welcome").classList.add("fade");
+        bodyDiv.style.display = "block";
+        await wait(500);
+        bodyDiv.classList.add("fade");
+        await wait(1500);
+    }
+
+    Cookies.set("do-fade", "1", { "expires": 2 });
+}
