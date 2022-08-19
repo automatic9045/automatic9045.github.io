c6212b691d1d9e6de56717ae44f87f9f7a5871d1
automatic9045 <67314487+automatic9045@users.noreply.github.com>
2022-08-19 22:15:34 +0900

    add: [AtsEX]PIのサンプルのコードとして他列車操作サンプルを追加

---
+++ contents/bve/AtsEX/codes/train-controller.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+using Automatic9045.AtsEx.PluginHost;
+using Automatic9045.AtsEx.PluginHost.ClassWrappers;
+using Automatic9045.AtsEx.PluginHost.Input.Native;
+using Automatic9045.AtsEx.PluginHost.Plugins;
+
+namespace Automatic9045.MapPlugins.TrainController {
+    public class TrainController : AssemblyPluginBase, IDisposable {
+        private Train Train;
+        private double Speed = 25 / 3.6; // 25[km/h] = (25 / 3.6)[m/s]
+
+        public TrainController(PluginBuilder builder) : base(builder, PluginType.MapPlugin) {
+            BveHacker.ScenarioCreated += e => Train = e.Scenario.Trains["test"];
+
+            App.NativeKeys.AtsKeys[NativeAtsKeyName.D].Pressed += OnDPressed;
+            App.NativeKeys.AtsKeys[NativeAtsKeyName.E].Pressed += OnEPressed;
+        }
+
+        private void OnDPressed(object sender, EventArgs e) => Train.TrainInfo.TrackKey = "1";
+        private void OnEPressed(object sender, EventArgs e) => Train.TrainInfo.TrackKey = "0";
+
+        public void Dispose() {
+            App.NativeKeys.AtsKeys[NativeAtsKeyName.D].Pressed -= OnDPressed;
+            App.NativeKeys.AtsKeys[NativeAtsKeyName.E].Pressed -= OnEPressed;
+        }
+
+        public override TickResult Tick(TimeSpan elapsed) {
+            if (App.NativeKeys.AtsKeys[NativeAtsKeyName.F].IsPressed) Speed -= 10.0 * elapsed.Ticks / TimeSpan.TicksPerMillisecond / 1000;
+            if (App.NativeKeys.AtsKeys[NativeAtsKeyName.G].IsPressed) Speed += 10.0 * elapsed.Ticks / TimeSpan.TicksPerMillisecond / 1000;
+
+            if (Speed > 0)
+            {
+                Speed -= 2.0 * elapsed.Ticks / TimeSpan.TicksPerMillisecond / 1000;
+                if (Speed < 0) Speed = 0;
+            }
+            else if (Speed < 0)
+            {
+                Speed += 2.0 * elapsed.Ticks / TimeSpan.TicksPerMillisecond / 1000;
+                if (Speed > 0) Speed = 0;
+            }
+
+            Train.Location += Speed * elapsed.Ticks / TimeSpan.TicksPerMillisecond / 1000;
+            Train.Speed = Speed;
+
+            return new MapPluginTickResult();
+        }
+    }
+}
