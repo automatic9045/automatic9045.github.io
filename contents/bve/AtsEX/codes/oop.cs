fee70e0b7d1c53683e5bab86f7c0a964953d5d3b
automatic9045 <67314487+automatic9045@users.noreply.github.com>
2022-08-19 22:12:39 +0900

    update: [AtsEX]PIのサンプルのコードを最新のものに更新

--- contents/bve/AtsEX/codes/oop.cs
+++ contents/bve/AtsEX/codes/oop.cs
@@ -1,20 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 using Automatic9045.AtsEx.PluginHost;
-using Automatic9045.AtsEx.PluginHost.Helpers;
+using Automatic9045.AtsEx.PluginHost.Handles;
+using Automatic9045.AtsEx.PluginHost.Plugins;
 
 namespace Automatic9045.VehiclePlugins.StateViewer {
     // 規定のクラスを継承することでプラグインとみなされます
-    public class StateViewer : AtsExPluginBase, IDisposable {
-        private StateForm Form;
-        private ToolStripMenuItem MenuItem;
+    public class StateViewer : AssemblyPluginBase, IDisposable {
+        private readonly StateForm Form;
+        private readonly ToolStripMenuItem MenuItem;
 
-        public StateViewer(HostServiceCollection services) : base(services) {
+        public StateViewer(PluginBuilder services) : base(services, PluginType.VehiclePlugin) {
             InstanceStore.Initialize(App, BveHacker);
 
             // 右クリックメニューに独自のメニューを追加可能
-            MenuItem = ContextMenuHacker.Instance.AddCheckableMenuItem("状態ウィンドウを表示", MenuItemCheckedChanged);
+            MenuItem = BveHacker.ContextMenuHacker.AddCheckableMenuItem("状態ウィンドウを表示", MenuItemCheckedChanged);
 
             MenuItem.Checked = false;
 
@@ -27,8 +32,9 @@ namespace Automatic9045.VehiclePlugins.StateViewer {
             BveHacker.MainFormSource.Focus();
         }
 
-        public override void Tick() {
+        public override TickResult Tick(TimeSpan elapsed) {
             Form?.Tick();
+            return new VehiclePluginTickResult(HandleCommandSet.DoNothing);
         }
 
         private void MenuItemCheckedChanged(object sender, EventArgs e) {
