a281037ed39d9c93288a143c1d476ab49ed9a8f7
automatic9045 <67314487+automatic9045@users.noreply.github.com>
2023-01-02 02:38:12 +0900

    update: HN、ユーザー名を全て小文字のものに書き換え

--- js/copyright.js
+++ js/copyright.js
@@ -1,3 +1,3 @@
 ﻿const year = new Date().getFullYear();
-const content = "&copy; " + (year == 2021 ? "2021" : "2021-" + year) + " Automatic9045";
+const content = "&copy; " + (year == 2021 ? "2021" : "2021-" + year) + " automatic9045";
 setHTMLByClasses(content, "footer-basic-copyright");
