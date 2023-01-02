c14863e9a560198b9aea7c7c7e0ae8ac51a8bd9f
automatic9045 <67314487+automatic9045@users.noreply.github.com>
2023-01-02 16:29:30 +0900

    dpdate: [AtsEX]紹介を最新バージョンのAtsEXに準拠

--- contents/bve/AtsEX/codes/oop.cs
+++ contents/bve/AtsEX/codes/oop.cs
@@ -1,25 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
-using Automatic9045.AtsEx.PluginHost;
-using Automatic9045.AtsEx.PluginHost.Handles;
-using Automatic9045.AtsEx.PluginHost.Plugins;
+using AtsEx.Extensions.ContextMenuHacker;
+using AtsEx.PluginHost.Plugins;
 
-namespace Automatic9045.VehiclePlugins.StateViewer {
+namespace AtsEx.Samples.VehiclePlugins.StateViewer {
     // 規定のクラスを継承することでプラグインとみなされます
-    public class StateViewer : AssemblyPluginBase, IDisposable {
+    [PluginType(PluginType.VehiclePlugin)]
+    public class StateViewer : AssemblyPluginBase {
         private readonly StateForm Form;
         private readonly ToolStripMenuItem MenuItem;
 
-        public StateViewer(PluginBuilder services) : base(services, PluginType.VehiclePlugin) {
-            InstanceStore.Initialize(App, BveHacker);
+        public StateViewer(PluginBuilder services) : base(services) {
+            InstanceStore.Initialize(Native, BveHacker);
 
             // 右クリックメニューに独自のメニューを追加可能
-            MenuItem = BveHacker.ContextMenuHacker.AddCheckableMenuItem("状態ウィンドウを表示", MenuItemCheckedChanged);
+            IContextMenuHacker contextMenuHacker = Extensions.GetExtension&lt;IContextMenuHacker&gt;();
+            MenuItem = contextMenuHacker.AddCheckableMenuItem("状態ウィンドウを表示", MenuItemCheckedChanged, ContextMenuItemType.Plugins);
 
             MenuItem.Checked = false;
 
@@ -32,9 +29,16 @@ namespace Automatic9045.VehiclePlugins.StateViewer {
             BveHacker.MainFormSource.Focus();
         }
 
+        public override void Dispose() {
+            Form.FormClosing -= FormClosing;
+            Form.Close();
+            Form.Dispose();
+        }
+
         public override TickResult Tick(TimeSpan elapsed) {
             Form?.Tick();
-            return new VehiclePluginTickResult(HandleCommandSet.DoNothing);
+
+            return new VehiclePluginTickResult();
         }
 
         private void MenuItemCheckedChanged(object sender, EventArgs e) {
@@ -49,11 +53,5 @@ namespace Automatic9045.VehiclePlugins.StateViewer {
             e.Cancel = true;
             MenuItem.Checked = false;
         }
-
-        public void Dispose() {
-            Form.FormClosing -= FormClosing;
-            Form.Close();
-            Form.Dispose();
-        }
     }
-}
+}   
--- contents/bve/AtsEX/codes/train-controller.cs
+++ contents/bve/AtsEX/codes/train-controller.cs
@@ -1,43 +1,44 @@
 ﻿using System;
 
-using Automatic9045.AtsEx.PluginHost;
-using Automatic9045.AtsEx.PluginHost.ClassWrappers;
-using Automatic9045.AtsEx.PluginHost.Input.Native;
-using Automatic9045.AtsEx.PluginHost.Plugins;
+using BveTypes.ClassWrappers;
 
-namespace Automatic9045.MapPlugins.TrainController {
-    public class TrainController : AssemblyPluginBase, IDisposable {
+using AtsEx.PluginHost;
+using AtsEx.PluginHost.Input.Native;
+using AtsEx.PluginHost.Plugins;
+
+namespace AtsEx.Samples.MapPlugins.TrainController {
+    // 規定のクラスを継承することでプラグインとみなされます
+    [PluginType(PluginType.MapPlugin)]
+    public class TrainController : AssemblyPluginBase {
         private Train Train;
         private double Speed = 25 / 3.6; // 25[km/h] = (25 / 3.6)[m/s]
 
-        public TrainController(PluginBuilder builder) : base(builder, PluginType.MapPlugin) {
+        public TrainController(PluginBuilder builder) : base(builder) {
             BveHacker.ScenarioCreated += e => Train = e.Scenario.Trains["test"];
 
-            App.NativeKeys.AtsKeys[NativeAtsKeyName.D].Pressed += OnDPressed;
-            App.NativeKeys.AtsKeys[NativeAtsKeyName.E].Pressed += OnEPressed;
+            Native.NativeKeys.AtsKeys[NativeAtsKeyName.D].Pressed += OnDPressed;
+            Native.NativeKeys.AtsKeys[NativeAtsKeyName.E].Pressed += OnEPressed;
         }
 
         private void OnDPressed(object sender, EventArgs e) => Train.TrainInfo.TrackKey = "1";
         private void OnEPressed(object sender, EventArgs e) => Train.TrainInfo.TrackKey = "0";
 
-        public void Dispose() {
-            App.NativeKeys.AtsKeys[NativeAtsKeyName.D].Pressed -= OnDPressed;
-            App.NativeKeys.AtsKeys[NativeAtsKeyName.E].Pressed -= OnEPressed;
+        public override void Dispose() {
+            Native.NativeKeys.AtsKeys[NativeAtsKeyName.D].Pressed -= OnDPressed;
+            Native.NativeKeys.AtsKeys[NativeAtsKeyName.E].Pressed -= OnEPressed;
         }
 
         public override TickResult Tick(TimeSpan elapsed) {
-            if (App.NativeKeys.AtsKeys[NativeAtsKeyName.F].IsPressed) Speed -= 10.0 * elapsed.Ticks / TimeSpan.TicksPerMillisecond / 1000;
-            if (App.NativeKeys.AtsKeys[NativeAtsKeyName.G].IsPressed) Speed += 10.0 * elapsed.Ticks / TimeSpan.TicksPerMillisecond / 1000;
+            if (Native.NativeKeys.AtsKeys[NativeAtsKeyName.F].IsPressed) Speed -= 10.0 * elapsed.Ticks / TimeSpan.TicksPerMillisecond / 1000;
+            if (Native.NativeKeys.AtsKeys[NativeAtsKeyName.G].IsPressed) Speed += 10.0 * elapsed.Ticks / TimeSpan.TicksPerMillisecond / 1000;
 
-            if (Speed > 0)
-            {
+            if (Speed > 0) {
                 Speed -= 2.0 * elapsed.Ticks / TimeSpan.TicksPerMillisecond / 1000;
-                if (Speed < 0) Speed = 0;
+                if (Speed < 0) Speed = 0d;
             }
-            else if (Speed < 0)
-            {
+            else if (Speed < 0) {
                 Speed += 2.0 * elapsed.Ticks / TimeSpan.TicksPerMillisecond / 1000;
-                if (Speed > 0) Speed = 0;
+                if (Speed > 0) Speed = 0d;
             }
 
             Train.Location += Speed * elapsed.Ticks / TimeSpan.TicksPerMillisecond / 1000;
