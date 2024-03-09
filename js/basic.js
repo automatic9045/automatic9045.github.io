ce80d4c63094931a84645969edb7e9948dad88e5
automatic9045 <67314487+automatic9045@users.noreply.github.com>
2024-03-10 01:11:30 +0900

    add: [バス側面]9811、0113、0218、0222を追加

--- js/basic.js
+++ js/basic.js
@@ -188,7 +188,7 @@ function scrollTo(targetId) {
 }
 
 function getRandomBusNumbers(count = 1) {
-	let busNumbers = ["0039", "F8926", "F8926", "F8926", "F9001", "F9045", "F9045", "F9045", "F9045", "9303", "9303", "9304", "9304", "9310", "9510"];
+	let busNumbers = ["0039", "F8926", "F8926", "F9001", "F9045", "F9045", "F9045", "9303", "9303", "9304", "9304", "9310", "9510", "9811", "9811", "0113", "0218", "0222"];
 	const pickedBusNumbers = [];
 
 	for (let i = 0; i < count; i++) {
