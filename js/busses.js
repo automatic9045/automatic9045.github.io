a60c9ef561759c0a269adad9ed2103e70e808513
automatic9045 <67314487+automatic9045@users.noreply.github.com>
2024-03-24 14:20:38 +0900

    refactor: [バス側面]ランダム排出処理を別ファイルに分離

---
+++ js/busses.js
@@ -0,0 +1,15 @@
+﻿function getRandomBusNumbers(count = 1) {
+	let busNumbers = ["0039", "F8926", "F8926", "F9001", "F9045", "F9045", "F9045", "9303", "9303", "9304", "9304", "9310", "9510", "9811", "9811", "0113", "0218", "0222",
+		"9811", "9811", "9811", "9811", "9811", "9811", "9811", "9811", "9811", "9811", "9811", "9811", "9811", "9811", "9811", "9811", "9811", "9811"];
+	const pickedBusNumbers = [];
+
+	for (let i = 0; i < count; i++) {
+		const j = Math.floor(Math.random() * busNumbers.length);
+		const pickedBusNumber = busNumbers[j];
+
+		busNumbers = busNumbers.filter(busNumber => busNumber != pickedBusNumber);
+		pickedBusNumbers.push(pickedBusNumber);
+	}
+
+	return pickedBusNumbers;
+}
